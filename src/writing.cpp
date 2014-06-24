f83a3cddb09064725a53c92f41eecb26296933bc
bruno <blais.bruno@gmail.com>
2014-06-24 16:47:56 -0400

    Added the writing toolset library These contain simple routine that help in naming files and etc

---
+++ src/writing.cpp
@@ -0,0 +1,46 @@
+// Last Modified: Tue 24 Jun 2014 03:19:00 PM EDT
+/******************************************************************************************
+*
+*   Framework for the Statistical Analysis of Particle-Laden Flows
+*   
+*   
+*   Author  : Bruno Blais
+*   File    : writingTools.cpp
+*
+*   Description :   Contains numerous functions to help such as conversion in writing.
+*
+*******************************************************************************************/
+
+
+//*******************
+//  GENERAL INCLUDES
+//*******************
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+/********************
+* HEADER INCLUDES
+********************/
+
+#define PI 3.14159265359
+#define WIDTH 12
+#define PRES 7
+
+std::string writingIntToString(int number)
+{
+   std::stringstream ss;    //create a stringstream
+   ss << number;	    //add number to the stream
+   return ss.str();	    //return a string with the contents of the stream
+}
+
+std::string writingDoubleToString(double number)
+{
+   std::stringstream ss;    //create a stringstream
+   ss << number;	    //add number to the stream
+   return ss.str();	    //return a string with the contents of the stream
+}
---
+++ src/writing.h
@@ -0,0 +1,26 @@
+/******************************************************************************************
+*
+*   Framework for the Statistical Analysis of Particle-Laden Flows
+*   
+*   
+*   Author  : Bruno Blais
+*   File    : writing.h
+*
+*   Description :   
+*
+*******************************************************************************************/
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <fstream>
+
+#ifndef WRITING_H
+#define WRITING_H 
+
+std::string writingIntToString(int);
+std::string writingDoubleToString(double);
+
+#endif
